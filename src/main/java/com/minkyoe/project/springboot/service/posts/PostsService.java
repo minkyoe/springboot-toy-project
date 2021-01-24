5be9580d5c9c5ddfe3fc09e0fb417ef5497db2a1
minkyoe <ddal0924@naver.com>
2021-01-24 23:10:38 +0900

    수정 API 추가 및 테스트

--- src/main/java/com/minkyoe/project/springboot/service/posts/PostsService.java
+++ src/main/java/com/minkyoe/project/springboot/service/posts/PostsService.java
@@ -1,7 +1,9 @@
 package com.minkyoe.project.springboot.service.posts;
 
+import com.minkyoe.project.springboot.domain.posts.Posts;
 import com.minkyoe.project.springboot.domain.posts.PostsRepository;
 import com.minkyoe.project.springboot.web.dto.PostsSaveRequestDto;
+import com.minkyoe.project.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +16,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update (Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
     }
 }
--- src/main/java/com/minkyoe/project/springboot/web/PostsApiController.java
+++ src/main/java/com/minkyoe/project/springboot/web/PostsApiController.java
@@ -2,10 +2,9 @@ package com.minkyoe.project.springboot.web;
 
 import com.minkyoe.project.springboot.service.posts.PostsService;
 import com.minkyoe.project.springboot.web.dto.PostsSaveRequestDto;
+import com.minkyoe.project.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +14,10 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
